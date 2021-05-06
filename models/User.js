0d544db41823bbd635cb4d181bb554ca17e441dd
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-05-06 14:10:18 -0400

    user post route working

--- models/User.js
+++ models/User.js
@@ -2,9 +2,14 @@ const {
     Model,
     DataTypes
 } = require('sequelize');
-const sequelize = require('../config/connection');
 
-class User extends Model {}
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password); 
+    }
+}
 
 User.init({
     id: {
