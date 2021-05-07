b80b4927b55a34e26b6aed6d8bd7ec2fd5d56a61
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-05-06 23:56:11 -0400

    seeded posts appear on homepage

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const { Post } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Post } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const newPost = await Post.create({
       ...req.body,
--- models/Post.js
+++ models/Post.js
@@ -1,40 +1,44 @@
-const {
-    Model,
-    DataTypes
-} = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Post extends Model {}
 
-Post.init({
+Post.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     title: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     text: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
 
     author_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: "user",
-            key: "id",
-        }
-    }
-}, {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: "post",
-});
+  }
+);
 
 module.exports = Post;
